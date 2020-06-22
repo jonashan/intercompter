b73f24cb52f1b8c8c5a398c02974b04273ad660c
Jonas Steen Hansen <jonas@jonas-hansen.dk>
2020-06-22 20:23:44 +0200

    test

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  url = ENV['SELENIUM_REMOTE_URL']
-  Capybara.server_host = 'web'
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: { url: url }
+  parallelize(workers: 1)
+  
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400],
+                       options: {
+                         browser: :remote,
+                         url: "http://host.docker.internal:9515",
+                       }
 end
